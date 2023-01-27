d3213fbbcc9da9a360bab65772c57cfd529b1b6a
Russell Blickhan <rwblickhan@gmail.com>
2023-01-27 15:55:21 -0800

    Move over everything besides stork stuff

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "R. W. Blickhan";
+export const SITE_DESCRIPTION = "The personal site of R.W. Blickhan";
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,37 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function get(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const fictionPosts = await getCollection("fiction");
+  const essaysPosts = await getCollection("essays");
+  const technicalPosts = await getCollection("technical");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: fictionPosts
+      .map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.date,
+        description: post.data.description,
+        link: `/fiction/${post.slug}/`,
+      }))
+      .concat(
+        essaysPosts.map((post) => ({
+          title: post.data.title,
+          pubDate: post.data.date,
+          description: post.data.description,
+          link: `/essays/${post.slug}/`,
+        }))
+      )
+      .concat(
+        technicalPosts.map((post) => ({
+          title: post.data.title,
+          pubDate: post.data.date,
+          description: post.data.description,
+          link: `/technical/${post.slug}/`,
+        }))
+      ),
+  });
 }
