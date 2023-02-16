e5a14c5133ec4d581ba3902eb5955081bc5d8444
Russell Blickhan <rwblickhan@gmail.com>
2023-02-15 18:05:06 -0800

    Add misc to RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,6 +6,7 @@ export async function get(context) {
   const fictionPosts = await getCollection("fiction");
   const essaysPosts = await getCollection("essays");
   const technicalPosts = await getCollection("technical");
+  const miscPosts = await getCollection("misc");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
@@ -31,6 +32,14 @@ export async function get(context) {
           pubDate: post.data.lastUpdatedDate,
           description: post.data.description,
           link: `/technical/${post.slug}/`,
+        }))
+      )
+      .concat(
+        miscPosts.map((post) => ({
+          title: post.data.title,
+          pubDate: post.data.lastUpdatedDate,
+          description: post.data.description,
+          link: `/misc/${post.slug}/`,
         }))
       ),
   });
