b00ef40d89833757bcdd598c1ab1306da6fda8c6
Russell Blickhan <rwblickhan@gmail.com>
2023-08-23 16:03:18 -0700

    Clean up RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,55 +1,25 @@
-import rss from "@astrojs/rss";
+import rss, { RSSFeedItem } from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { collectionMetadataMap } from "../content/config";
 
 export async function get(context: any) {
-  const fictionPosts = await getCollection("fiction");
-  const essaysPosts = await getCollection("essays");
-  const technicalPosts = await getCollection("technical");
-  const tilPosts = await getCollection("til");
-  const miscPosts = await getCollection("misc");
+  const items: RSSFeedItem[] = [];
+  for (const [collection, metadata] of collectionMetadataMap.entries()) {
+    const posts = await getCollection(collection);
+    for (const post of posts) {
+      items.push({
+        title: post.data.title,
+        pubDate: post.data.lastUpdatedDate,
+        description: post.data.description,
+        link: `${metadata.slug}/${post.slug}/`,
+      });
+    }
+  }
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: fictionPosts
-      .map((post) => ({
-        title: post.data.title,
-        pubDate: post.data.lastUpdatedDate,
-        description: post.data.description,
-        link: `/fiction/${post.slug}/`,
-      }))
-      .concat(
-        essaysPosts.map((post) => ({
-          title: post.data.title,
-          pubDate: post.data.lastUpdatedDate,
-          description: post.data.description,
-          link: `/essays/${post.slug}/`,
-        }))
-      )
-      .concat(
-        technicalPosts.map((post) => ({
-          title: post.data.title,
-          pubDate: post.data.lastUpdatedDate,
-          description: post.data.description,
-          link: `/technical/${post.slug}/`,
-        }))
-      )
-      .concat(
-        tilPosts.map((post) => ({
-          title: post.data.title,
-          pubDate: post.data.lastUpdatedDate,
-          description: `TIL for ${post.data.lastUpdatedDate.toDateString()}`,
-          link: `/technical/til/${post.slug}/`,
-        }))
-      )
-      .concat(
-        miscPosts.map((post) => ({
-          title: post.data.title,
-          pubDate: post.data.lastUpdatedDate,
-          description: "",
-          link: `/misc/${post.slug}/`,
-        }))
-      ),
+    items,
   });
 }
