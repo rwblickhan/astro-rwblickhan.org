9b557697070c890cca899b72bcf28b6f80f27b61
Russell Blickhan <rwblickhan@gmail.com>
2023-09-19 20:05:13 +1000

    Remove custom search

--- src/consts.ts
+++ src/consts.ts
@@ -3,23 +3,6 @@ import type { Collection } from "./content/config";
 export const SITE_TITLE = "R. W. Blickhan";
 export const SITE_DESCRIPTION = "The personal site of R.W. Blickhan";
 
-export interface IndexEntry {
-  slug: string;
-  title: string;
-}
-
-export type SearchWorkerMessageType = "init" | "query";
-
-export type SearchWorkerMessage =
-  | {
-      type: "init";
-      index: IndexEntry[];
-    }
-  | {
-      type: "query";
-      query: string;
-    };
-
 export interface CollectionMetadata {
   title: string;
   slug: string;
