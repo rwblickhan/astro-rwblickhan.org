d69a8d9c13d4fa1531ad48d0079208ed62adb69e
Russell Blickhan <rwblickhan@gmail.com>
2023-12-31 13:18:05 -0800

    Remove the rest of tailwind in components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,8 +19,6 @@ module.exports = {
         "rwb-text-dark": "#e8e6e3",
         "rwb-link-light": "#3366cc",
         "rwb-link-dark": "#689fd9",
-        "rwb-button-background-dark": "#254a93",
-        "rwb-slate-light": "#ebedef",
       },
     },
   },
