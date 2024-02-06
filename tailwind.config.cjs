fdbd27290129bb916e4d8e87bbf01c89c0989fbb
Russell Blickhan <rwblickhan@gmail.com>
2024-02-05 23:04:53 -0800

    Clean up global styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,19 +3,8 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      // https://github.com/system-fonts/modern-font-stacks#transitional
-      serif: [
-        "Charter",
-        "'Bitstream Charter'",
-        "'Sitka Text'",
-        "Cambria",
-        ...defaultTheme.fontFamily.serif,
-      ],
-    },
     extend: {
       colors: {
-        "rwb-background-dark": "#101315",
         "rwb-text-dark": "#e8e6e3",
         "rwb-link-light": "#3366cc",
         "rwb-link-dark": "#689fd9",
