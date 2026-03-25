c89a0f47cfed1555e8636999b430b0b496803367
Russell Blickhan <rwb@rwblickhan.org>
2026-03-25 15:16:19 -0700

    Add atkinson hyperlegible mono for code and kbd blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import pagefind from "astro-pagefind";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
