a67573af9cc2500763dd6f1c69f591a1990ed1bb
Russell Blickhan <rwb@rwblickhan.org>
2026-04-13 21:14:00 -0700

    Add fancy scroll-spy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import pagefind from "astro-pagefind";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
 import remarkA11yEmoji from "@fec/remark-a11y-emoji";
 import rehypeBlockquoteFigures from "rehype-blockquote-figures";
 import rehypeFigcaption from "rehype-figcaption";
@@ -40,13 +39,6 @@ export default defineConfig({
     remarkPlugins: [
       // Generate a table of contents if we see a "Table of Contents" header
       [remarkToc, { maxDepth: 2 }],
-      [
-        // Collapse the generated table of contents into a <details> tag
-        remarkCollapse,
-        {
-          test: "Table of Contents",
-        },
-      ],
       remarkA11yEmoji,
       remarkDirective,
       remarkHtmlDirectives,
